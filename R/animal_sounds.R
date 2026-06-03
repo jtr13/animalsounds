d83526acfe48702cd114f4ffac7f61e26f33a85a
Heather Turner <ht@heatherturner.net>
2026-06-03 16:37:00 +0100

    document animal sounds

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,17 @@
+#' Title
+#'
+#' Description here
+#'
+#' This function uses [paste0()].
+#'
+#' @param animal a character string naming an animal
+#' @param sound a character string giving the animal sound
+#'
+#' @return a string of the form "The animal says sound!"
+#' @export
+#'
+#' @examples
+#' animal_sounds("dog", "woof")
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
